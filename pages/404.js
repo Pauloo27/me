a1bf2be37f0c98735bf3c12c2f3091d193534bc8
Paulo <pauloeduardo2709@gmail.com>
2021-03-28 16:46:29 -0300

    Add title header

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,28 @@
 import Link from "next/link";
+import Head from "next/head";
 import style from "../styles/404.module.css";
 
 export default function Error404() {
   return (
-    <div className={style.main_container}>
-      <div className={style.error_container}>
-        <h1 className={style.error_code}>404</h1>
-        <h3 className={style.error_message}>The page you'are looking for was not found</h3>
+    <>
+      <Head>
+        <title>404 - Page not found</title>
+      </Head>
+      <div className={style.main_container}>
+        <div className={style.error_container}>
+          <h1 className={style.error_code}>404</h1>
+          <h3 className={style.error_message}>The page you&apos;re looking for was not found</h3>
+        </div>
+        <Link href="/">
+          {/* TODO: random project */}
+          <a>Show me a random project</a>
+        </Link>
+        <img
+          src="https://media1.tenor.com/images/c47c76ddfeac126790c927d84b253183/tenor.gif?itemid=15689695"
+          alt="GIF of a confused man"
+          className={style.gif}
+        />
       </div>
-      <Link href="/">
-        <a>Take me home!</a>
-      </Link>
-      <img src="https://media1.tenor.com/images/c47c76ddfeac126790c927d84b253183/tenor.gif?itemid=15689695" className={style.gif}/>
-    </div>
+    </>
   );
 }
