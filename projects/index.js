e9448d8beab508978103f4212badaae6c911cf43
Paulo <pauloeduardo2709@gmail.com>
2021-03-28 18:11:49 -0300

    Add project list

---
+++ projects/index.js
@@ -0,0 +1,30 @@
+const react = { name: "React" };
+const nodejs = { name: "NodeJS" };
+const golang = { name: "Go" };
+
+const projects = [
+  {
+    name: "Homepage",
+    description: "A homepage with a ToDo, Trello and Google Calendar Integration",
+    thumbnail: "https://i.imgur.com/BGpE4nu.png",
+    source: "https://github.com/Pauloo27/homepage",
+    preview: "https://pauloo27.github.io/homepage",
+    madeWith: [react],
+  },
+  {
+    name: "Tuner",
+    description: "Search and Play songs from YouTube inside your terminal",
+    thumbnail: "https://i.imgur.com/REvt9Kw.png",
+    source: "https://github.com/Pauloo27/tuner",
+    madeWith: [react],
+  },
+  {
+    name: "Gotroller",
+    description: "MPRIS controller wrote in Go for Polybar with extra GUI",
+    thumbnail: "https://i.imgur.com/HrEGG2E.png",
+    source: "https://github.com/Pauloo27/gotroller",
+    madeWith: [react],
+  },
+];
+
+export default projects;
