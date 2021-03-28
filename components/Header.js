c4fb2ce118956abf798eeba2c671a78427bc881a
Paulo <pauloeduardo2709@gmail.com>
2021-03-28 16:19:31 -0300

    Add Header and page pages

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import style from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={style.container}>
+      <Link href="/">
+        <a className={style.home}>Home</a>
+      </Link>
+      <div>
+        <Link href="/projects">
+          <a className={style.item}>Projects</a>
+        </Link>
+        <Link href="/contact">
+          <a className={style.item}>Contact</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import "../styles/theme.css";
 import "../styles/globals.css";
+import Header from "../components/Header";
+import style from "../styles/App.module.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <main className={style.main_container}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
 export default MyApp;
