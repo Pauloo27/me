bd2ba1f57cb3cdd02d51af3d818d5afc5ec48cbe
Paulo <pauloeduardo2709@gmail.com>
2021-03-31 19:04:12 -0300

    Add FontAwesome and GitHub/GitLab links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faGitlab } from "@fortawesome/free-brands-svg-icons";
 import style from "../styles/Home.module.css";
 import ProjectsGroup from "../components/ProjectsGroup";
 import { group } from "../lib/utils";
@@ -13,6 +16,18 @@ export default function Home() {
       </Head>
       <div className={style.info_container}>
         <h1 className={style.name}>Paulo Eduardo</h1>
+        <div className={style.social_container}>
+          <Link href="https://www.github.com/Pauloo27/">
+            <a rel="noopener noreferrer" target="_blank" className={style.social_item}>
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </Link>
+          <Link href="https://www.gitlab.com/Pauloo27/">
+            <a rel="noopener noreferrer" target="_blank" className={style.social_item}>
+              <FontAwesomeIcon icon={faGitlab} />
+            </a>
+          </Link>
+        </div>
         <p className={style.description}>
           A Software Engineer that loves GoLang and uses Arch btw.
           Do you need to know more? Then look at some of my projects:
