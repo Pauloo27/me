5dff67fe17ff6bf2a36945e5b2051bcf03170ea2
Paulo <pauloeduardo2709@gmail.com>
2021-04-02 18:28:53 -0300

    Add title to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,21 @@
+import Head from "next/head";
 import style from "../styles/Contact.module.css";
 
 export default function Contact() {
   return (
-    <div className={style.container}>
-      <h1>Contact</h1>
-      <h2>Page under construction...</h2>
-      <img
-        src="/construction.gif"
-        className={style.gif}
-        alt="GIF of a confused man"
-      />
-    </div>
+    <>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <div className={style.container}>
+        <h1>Contact</h1>
+        <h2>Page under construction...</h2>
+        <img
+          src="/construction.gif"
+          className={style.gif}
+          alt="GIF of a confused man"
+        />
+      </div>
+    </>
   );
 }
