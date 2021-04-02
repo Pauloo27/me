98ff8e0a247f714380b01b4baaab751b4ab629a6
Paulo <pauloeduardo2709@gmail.com>
2021-04-02 18:01:10 -0300

    Add 'under construction' message in the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,15 @@
+import style from "../styles/Contact.module.css";
+
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div className={style.container}>
+      <h1>Contact</h1>
+      <h2>Page under construction...</h2>
+      <img
+        src="/construction.gif"
+        className={style.gif}
+        alt="GIF of a confused man"
+      />
+    </div>
+  );
 }
