d815f194f73407031616fee4b0408079e22c612a
Paulo <pauloeduardo2709@gmail.com>
2021-04-02 01:16:48 -0300

    Add more projects

--- projects/index.js
+++ projects/index.js
@@ -1,5 +1,6 @@
 const react = { name: "React" };
 const golang = { name: "Go" };
+const nodejs = { name: "NodeJS" };
 
 const projects = [
   {
@@ -22,6 +23,62 @@ const projects = [
     description: "MPRIS controller wrote in Go for Polybar with extra GUI",
     thumbnail: "https://i.imgur.com/ArxvWCB.png",
     source: "https://github.com/Pauloo27/gotroller",
+    madeWith: [golang],
+  },
+  {
+    name: "TicTacToe",
+    description: "Tic Tac Toe multiplayer",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/tictactoe",
+    madeWith: [react, nodejs],
+  },
+  {
+    name: "Go MPRIS",
+    description: "A Go library for MPRIS",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/go-mpris",
+    madeWith: [golang],
+  },
+  {
+    name: "TL;DR",
+    description: "TL;DR pages fetcher",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/tldr",
+    madeWith: [golang],
+  },
+  {
+    name: "Morcego",
+    description: "Blind SQLI Tool to fetch size and data",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/morcego",
+    madeWith: [golang],
+  },
+  {
+    name: "Snek",
+    description: "Web version of the Sneak game",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/snek",
+    madeWith: [golang],
+  },
+  {
+    name: "MediaEnem",
+    description: "Enem grade calculator",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/MediaEnem",
+    madeWith: [golang],
+  },
+  {
+    name: "Reddit",
+    description: "Reddit Video Downloader library",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/reddit",
+    madeWith: [golang],
+  },
+  {
+    name: "TrucoReact",
+    description: "Web version of the card game 'Truco'",
+    thumbnail: "https://i.imgur.com/ArxvWCB.png",
+    source: "https://github.com/Pauloo27/TrucoReact",
     madeWith: [golang],
   },
 ];
