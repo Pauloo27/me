487e373d82dcaeba0e8ce1fd6bd80df12eaee705
Paulo <pauloeduardo2709@gmail.com>
2021-04-02 20:19:18 -0300

    Add metadata tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/theme.css";
 import "../styles/globals.css";
+import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import style from "../styles/App.module.css";
@@ -7,6 +8,14 @@ import style from "../styles/App.module.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta property="og:site_name" content="Pauloo27" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Me, Paulo" />
+        <meta property="og:description" content="My portfolio" />
+        <meta property="og:url" content="https://pauloo27.dev" />
+        <meta property="og:image" content="/icon.png" />
+      </Head>
       <Header />
       <main className={style.main_container}>
         <Component {...pageProps} />
