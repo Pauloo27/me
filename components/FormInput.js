bb64aba5e695cf66f7419bb2048b98ac519aaa84
Paulo <pauloeduardo2709@gmail.com>
2021-04-22 22:01:15 -0300

    Add form validation

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,27 +1,40 @@
 import style from "../styles/Form.module.css";
 
-export default function FormInput({ name, placeholder, store, rows }) {
+export default function FormInput({
+  name, placeholder, store, rows, errors,
+}) {
   const handleChange = (e) => {
     store(name, e.target.value);
   };
 
-  if (rows !== undefined && rows >= 2)
+  const error = errors?.[name];
+
+  const getInput = () => {
+    if (rows !== undefined && rows >= 2)
+      return (
+        <textarea
+          rows={rows}
+          className={style.text_area}
+          name={name}
+          onChange={handleChange}
+          placeholder={placeholder}
+        />
+      );
+
     return (
-      <textarea
-        rows={rows}
-        className={style.text_area}
+      <input
+        className={style.text_input}
         name={name}
         onChange={handleChange}
         placeholder={placeholder}
       />
     );
+  };
 
   return (
-    <input
-      className={style.text_input}
-      name={name}
-      onChange={handleChange}
-      placeholder={placeholder}
-    />
+    <div className={style.input_container}>
+      <span className={style.input_error}>{error}</span>
+      {getInput()}
+    </div>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { validate, contactConstraint } from "../lib/validate";
 import { useState, useEffect } from "react";
 import Form from "../components/Form";
 import FormSubmit from "../components/FormSubmit";
@@ -16,6 +17,7 @@ export default function Contact() {
     setTimeout(fetchEmail, 1000);
   }, []);
 
+  const [validatonErrors, setValidationErrors] = useState(undefined);
   const [store, setStore] = useState({});
 
   const updateStore = (key, value) => {
@@ -23,10 +25,14 @@ export default function Contact() {
     setStore(newStore);
   };
 
+  // Validation (called every time store is updated)
+  useEffect(() => {
+    setValidationErrors(validate(store, contactConstraint));
+  }, [store, setValidationErrors]);
+
   const onSubmit = (event) => {
-    // TODO: verify
+    // TODO: validate
     // TODO: call api
-    console.log("the input data is", store);
   };
 
   return (
@@ -46,11 +52,11 @@ export default function Contact() {
         <h3 className={style.text_center}>Or this form:</h3>
         <Form onSubmit={onSubmit}>
           <FormGroup>
-            <FormInput store={updateStore} name="name" placeholder="Your name" />
-            <FormInput store={updateStore} name="email" placeholder="Your email" />
+            <FormInput errors={validatonErrors} store={updateStore} name="name" placeholder="Your name" />
+            <FormInput errors={validatonErrors} store={updateStore} name="email" placeholder="Your email" />
           </FormGroup>
-          <FormInput store={updateStore} name="subject" placeholder="Subject" />
-          <FormInput rows={5} store={updateStore} name="message" placeholder="Message" />
+          <FormInput errors={validatonErrors} store={updateStore} name="subject" placeholder="Subject" />
+          <FormInput errors={validatonErrors} rows={5} store={updateStore} name="message" placeholder="Message" />
           <FormSubmit />
         </Form>
       </div>
