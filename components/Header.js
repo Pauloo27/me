c5695ba8714a5a6d8aa1376c5876befff5e4ef93
Paulo <git@pauloo27.dev>
2021-11-01 22:47:33 -0300

    ux: highlight selected page in header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,25 @@
 import Link from "next/link";
 import style from "../styles/Header.module.css";
+import { useRouter } from "next/router";
+import cn from "classnames";
+
+const routes = [
+  { path: "/", name: "Home" },
+  { path: "/contact", name: "Contact" },
+];
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className={style.container}>
-      <Link href="/">
-        <a className={style.item}>Home</a>
-      </Link>
-      <div>
-        <Link href="/contact">
-          <a className={style.item}>Contact</a>
-        </Link>
-      </div>
-    </header>
+      {routes.map(route => {
+        return (
+          <Link key={route.name} href={route.path} >
+            <a className={cn(style.item, { [style.selected_item]: router.route === route.path })}>{route.name}</a>
+          </Link>
+        )
+      })}
+    </header >
   );
 }
