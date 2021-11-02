27f45aacd34ec64af1aab7429a15cfe4f515e892
Paulo <git@pauloo27.dev>
2021-11-01 22:49:41 -0300

    dep: upgrade class names

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import style from "../styles/Header.module.css";
 import { useRouter } from "next/router";
 import cn from "classnames";
+import style from "../styles/Header.module.css";
 
 const routes = [
   { path: "/", name: "Home" },
@@ -13,13 +13,16 @@ export default function Header() {
 
   return (
     <header className={style.container}>
-      {routes.map(route => {
-        return (
-          <Link key={route.name} href={route.path} >
-            <a className={cn(style.item, { [style.selected_item]: router.route === route.path })}>{route.name}</a>
-          </Link>
-        )
-      })}
-    </header >
+      {routes.map((route) => (
+        <Link key={route.name} href={route.path}>
+          <a className={
+            cn(style.item, { [style.selected_item]: router.route === route.path })
+          }
+          >
+            {route.name}
+          </a>
+        </Link>
+      ))}
+    </header>
   );
 }
