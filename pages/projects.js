44aa2bbc7d2068743fe1bf920cdceb451b0c78ae
Paulo <git@pauloo27.dev>
2022-04-23 11:03:47 -0300

    feat(i18n): translate projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,17 +1,29 @@
 import Head from "next/head";
 import commonStyle from "@styles/Common.module.css";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function Projects() {
+  const { t } = useTranslation("projects");
+
   return (
     <>
       <Head>
-        <title>Projects</title>
+        <title>{t("pageTitle")}</title>
       </Head>
       <div className={commonStyle.page_container}>
         <h1 className={commonStyle.page_title}>
-          Work in Progress
+          {t("wip")}
         </h1>
       </div>
     </>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["projects"])),
+    },
+  };
 }
